56e33d2ef46fca25fe86383dd9d26f22e572753a
SETSUIKI HYORYU <tayutai@outlook.jp>
2021-08-08 10:00:52 +0900

    fix: 初期化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h2>{{ unite }}</h2>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+  data() {
+    return {
+      unite: "全世界的無産者，聯合起來！",
+    };
   },
 });
 </script>
